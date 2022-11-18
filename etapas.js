f524ff7ed8c95aaaa0c88982465b5b95ea171f04
franciscalorraynes <franciscalorrayne5@gmail.com>
2022-11-18 14:38:20 -0300

    terminando projeto urna

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numero: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        name: 'Fulano de Tal',
+        partido: 'ABC',
+        fotos: [{ URL: 'vereador.png', legenda: 'Vereador' }]
+      },
+      {
+        numero: 771313,
+        name: 'Beltrano Duarte',
+        partido: 'DEFG',
+        fotos: [{ URL: 'vereadora.png', legenda: 'Vereadora' }]
+      }
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        name: 'Ciclano',
+        partido: 'ABC',
+        vice: 'Circ',
+        fotos: [
+          { URL: 'prefeito1.png', legenda: 'Prefeito' },
+          { URL: 'viceprefeiro1.png', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+      {
+        numero: 84,
+        name: 'Zulano',
+        partido: 'QWERTY',
+        vice: 'Zul',
+        fotos: [
+          { URL: 'prefeirto2.png', legenda: 'Prefeito' },
+          { URL: 'vice2.png', legenda: 'Vice-Prefeito', small: true }
+        ]
+      }
+    ]
+  }
+]
---
+++ script.js
@@ -0,0 +1,3 @@
+let SeuVotoPara = document.querySelector('.divisao-1-1 span')
+let cargo = document.querySelector('.divisao-1-2 span')
+let des = document.querySelector('.divisao-1-4 span')
